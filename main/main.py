1dd5ac3ea1da294c2a426c6a16c5dad4f572bbe0
d-marretta <danielemarretta@gmail.com>
2024-12-30 00:14:00 +0100

    Refactored

--- main/main.py
+++ main/main.py
@@ -1,49 +1,133 @@
+import chess.svg
 import cv2
-import os
 import numpy as np
 import math
-im = cv2.imread('datasets/detection_dataset/images/test/G000_IMG000.jpg')
-height, width, _ = im.shape
-coords = [0.15908203125, 0.35113932291666666, 0.5768977864583333, 0.20787434895833334, 0.84970703125, 0.5081054687500001, 0.34612630208333334, 0.7500325520833333]
+import chess
+import cairosvg
+import ultralytics
 
-top_left = coords[0] * width, coords[1] * height
-top_right = coords[2] * width, coords[3] * height
-bottom_right = coords[4] * width, coords[5] * height
-bottom_left = coords[6] * width, coords[7] * height
+def get_coords_chessboards(model, image_path):
+    results = model(image_path)
+    result = results[0]
+    contour = result.masks.xy.pop()
 
-side_chessboard = int(math.sqrt((top_right[0] - top_left[0]) ** 2 + (top_right[1] - top_right[1]) ** 2))
+    contour = contour.astype(np.int32)
 
-input_pts = np.float32([top_left, top_right, bottom_right, bottom_left])
-dest_pts = np.float32([[0,0], [side_chessboard, 0], [side_chessboard, side_chessboard], [0, side_chessboard] ])
+    contour = contour.reshape(-1, 1, 2)
+    epsilon = 0.02*cv2.arcLength(contour, False)
+    vertices = cv2.approxPolyDP(contour, epsilon, closed=True)  
+    vertices = np.squeeze(vertices)
+    vertices = order_vertices(vertices)
+    
+    return vertices
+    
 
-M = cv2.getPerspectiveTransform(input_pts, dest_pts)
+def order_vertices(vertices):
+    sorted_vertices = sorted(vertices, key=lambda v: (-v[0],v[1]))
+    right_vertices = sorted_vertices[:2]
+    left_vertices = sorted_vertices[2:]
 
-warped = cv2.warpPerspective(im, M, (side_chessboard, side_chessboard))
+    top_right = min(right_vertices, key=lambda v: v[1])
+    bottom_right = max(right_vertices, key=lambda v: v[1])
 
-n_squares = 8
-square_size = side_chessboard / n_squares
+    top_left = min(left_vertices, key=lambda v: v[1])
+    bottom_left = max(left_vertices, key=lambda v: v[1])
 
-grid_lines = warped.copy()
+    return top_left, top_right, bottom_right, bottom_left
 
-for i in range(n_squares + 1):
-    y = int(i * square_size)
-    cv2.line(grid_lines, (0, y), (side_chessboard, y), (0, 0, 255), 1)
+def warp_chessboard(vertices):
+    top_left, top_right, bottom_right, bottom_left = vertices
+    n_squares = 8
 
-for i in range(n_squares + 1):
-    x = int(i * square_size)
-    cv2.line(grid_lines, (x, 0), (x, side_chessboard), (0, 0, 255), 1)  
+    side_chessboard = int(math.sqrt((top_right[0] - top_left[0]) ** 2 + (top_right[1] - top_right[1]) ** 2))
+
+    input_pts = np.float32([top_left, top_right, bottom_right, bottom_left])
+    dest_pts = np.float32([[0,0], [side_chessboard, 0], [side_chessboard, side_chessboard], [0, side_chessboard] ])
+
+    M = cv2.getPerspectiveTransform(input_pts, dest_pts)
+    square_size = side_chessboard / n_squares
+
+    return M, square_size
 
 
-bbox = [0.41768229166666665, 0.24734700520833333, 0.044514973958333336, 0.09892578125]
+def get_bboxes(model, image_path, M, square_size):
+    classes = { 0:"P",
+                1:"R",
+                2:"N",
+                3:"B",
+                4:"Q",
+                5:"K",
+                6:"p",
+                7:"r",
+                8:"n",
+                9:"b",
+                10:"q",
+                11:"k"}
+    
+    results = model(image_path,conf=0.30)
+    result = results[0]
+    bboxs = []
+    for box in result.boxes:
+        # print(int(box.cls), float(box.conf))
+        b = box.xywh[0]
+        b_class = classes[int(box.cls)]
+        bboxs.append((b_class, b))
 
-x_center, y_center, box_width, box_height = bbox[0]*width, bbox[1]*height, bbox[2]*width, bbox[3]*height
-original_point = np.array([[x_center, y_center+box_height//2.3]], dtype='float32')  # Shape (1, 1, 2)
-original_point = np.array([original_point])  # Adding batch dimension for cv2.perspectiveTransform
+    original_points = np.array([[[bbox[1][0], bbox[1][1]+bbox[1][3]//2.4]] for bbox in bboxs], dtype='float32')
 
-transformed_point = cv2.perspectiveTransform(original_point, M)
+    transformed_points = cv2.perspectiveTransform(original_points, M)
 
-transformed_x, transformed_y = transformed_point[0][0][0], transformed_point[0][0][1]
+    positions = []
+    for i, point in enumerate(transformed_points):
+        transformed_x, transformed_y = point[0][0], point[0][1]    
+        
+        square_col = int(transformed_x // square_size)
+        square_row = int(transformed_y // square_size)+1
+        piece_pos = (8-square_row, square_col, bboxs[i][0])
+        positions.append(piece_pos)
 
-cv2.circle(grid_lines, (int(transformed_x),int(transformed_y)), 3, (0,0,255), -1)
-cv2.imwrite('prova.jpg', grid_lines)
+    positions.sort(key=lambda x: (-x[0],x[1]))
 
+    return positions
+
+def get_fen_board(positions):
+    board = [["" for _ in range(8)] for _ in range(8)]
+
+    for row, col, piece_type in positions:
+        board[row][col] = piece_type
+
+    fen_rows = []
+    for row in reversed(board):
+        fen_row = ""
+        empty_count = 0
+        for cell in row:
+            if cell == "":
+                empty_count += 1
+            else:
+                if empty_count > 0:
+                    fen_row += str(empty_count)
+                    empty_count = 0
+                fen_row += cell
+        if empty_count > 0:
+            fen_row += str(empty_count)
+        fen_rows.append(fen_row)
+
+    fen_board = "/".join(fen_rows)
+
+    return fen_board
+
+def main(seg_yolo, detect_yolo, image_path):
+    vertices = get_coords_chessboards(seg_yolo, image_path)
+    M, square_size = warp_chessboard(vertices)
+    positions = get_bboxes(detect_yolo, image_path, M, square_size)
+    fen_board = get_fen_board(positions)
+
+    board = chess.Board(fen_board)
+    cairosvg.svg2png(chess.svg.board(board, size=1200),write_to='prova.png')
+
+
+if __name__ == '__main__':
+    seg_yolo = ultralytics.YOLO('./models/yolo11n-seg-best.pt')
+    detect_yolo = ultralytics.YOLO('./models/yolo11n-best.pt')
+    image_path = 'datasets/detection_dataset/images/test/G076_IMG010.jpg'
+    main(seg_yolo, detect_yolo, image_path)
